c591e2e3bb3b23b804cbd658486fc71bb22947da
mary <maryam.nokohan@gmail.com>
2025-09-06 13:04:47 +0330

    (feat): Added a implemtion for GetCity to get the city detail from open meto

---
+++ backend/internal/weather/cities.go
@@ -0,0 +1,42 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"github.com/maryam-nokohan/WeatherApp/backend/internal/models"
+)
+
+
+func GetCity(cityName string)(*models.City , error){
+ url := fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1", cityName)
+
+    resp, err := http.Get(url)
+	if err != nil{
+		return  nil, fmt.Errorf("faild to fetch th coordinates")
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+			return  nil, fmt.Errorf("geocoding api error : %s" , resp.Status)
+	}
+
+	body , err := io.ReadAll(resp.Body)
+	if err != nil {
+		return  nil, fmt.Errorf("faild to read the response")
+	}
+
+	var geo models.GeoResponse
+
+	if err := json.Unmarshal(body , &geo); err != nil {
+		return nil , fmt.Errorf("faild to unmarshal the json api")
+	}
+
+	if len(geo.Results) == 0 {
+		return  nil , fmt.Errorf("city not found: %s" , cityName)
+	}
+	return &geo.Results[0] , nil
+
+}
